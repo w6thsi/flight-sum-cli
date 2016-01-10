cb7ee92f772d10cc3284963178eac867a8e87b9b
Wathsala Werapitiya <wathsala.werapitiya@kaplan.com>
2016-01-10 20:24:23 +0000

    Made it work

--- test/generator.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-
-describe('Generator Test', function () {
-  var input = "";
-
-  it('should out put this data', function () {
-    expect('foo').to.not.equal('bar');
-  });
-});
