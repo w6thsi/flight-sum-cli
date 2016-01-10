c3f9f90d9d63c3f19847a17cc95c7ca12ce17add
Wathsala Werapitiya <wathsala.werapitiya@kaplan.com>
2016-01-10 17:40:11 +0000

    First commit

---
+++ test/generator.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('Generator Test', function () {
+  var input = "";
+
+  it('should out put this data', function () {
+    expect('foo').to.not.equal('bar');
+  });
+});
